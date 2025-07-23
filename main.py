9b8383ad64839f81e15502780edab47e41f16719
Blackout <58275399+chuparola@users.noreply.github.com>
2025-07-22 22:40:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
-    page.goto("https://example.com")
+    page.goto("https://google.com")
     print(page.title())
     browser.close()
